d5123d8239df14a1acd401b2efa977c80b8af2d0
ntrakau <ntrakau@gmail.com>
2024-02-01 18:08:43 +0200

    Chapter 4 Numbers in Lists

---
+++ Chapter_4 = Working with Lists/even_numbers.py
@@ -0,0 +1,5 @@
+#NT Rakau
+
+#range()prints out the multiple of 3or skip number by the 3rd value passed in the range args
+even_numbers = list(range(2,11,2))
+print(even_numbers)
---
+++ Chapter_4 = Working with Lists/numbers.py
@@ -0,0 +1,8 @@
+#NT Rakau
+#Making numerical lists
+
+for value in range(1,5):#range()function counts from the first arg to and not through the sencond arg
+    print(value)
+
+numbers = list(range(1,7))
+print(numbers)
